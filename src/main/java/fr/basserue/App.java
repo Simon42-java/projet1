e188fc9983cc02bb1fb9832e94c282b1fa3d3ebb
Simon42-java <simon.basserue@ltpdampierre.fr>
2021-02-12 08:59:01 +0100

    test commit

--- src/main/java/fr/basserue/App.java
+++ src/main/java/fr/basserue/App.java
@@ -20,6 +20,7 @@ public class App extends Application {
         scene = new Scene(loadFXML("SIMONBOUON"), 1920, 1080);
         stage.setScene(scene);
         stage.show();
+        
     }
 
     static void setRoot(String fxml) throws IOException {
